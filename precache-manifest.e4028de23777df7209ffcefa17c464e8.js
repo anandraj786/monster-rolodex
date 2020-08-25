b46e8e411f21bef9cf388ae20e723ebe5aacaa0a
Anand <anandraja1993.15@gmail.com>
2020-08-25 09:04:37 +0530

    Updates

---
+++ precache-manifest.e4028de23777df7209ffcefa17c464e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12ae772ca7092e0368405ac76eddb72",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "404a06ba7f30d20dda7a",
+    "url": "/monster-rolodex/static/css/main.eb7bf04f.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404a06ba7f30d20dda7a",
+    "url": "/monster-rolodex/static/js/main.881d709b.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
